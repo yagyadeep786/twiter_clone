57f1a8a6e1248d1f4cca20603494074731aeef9d
yagyadeep ahirwar <136895856+yagyadeep786@users.noreply.github.com>
2025-01-07 13:01:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Toaster} from "react-hot-toast"
 function App() {
  // this is my line confilct work testing from twiter_f branch
 // here is the main app (testing)
+ // second case line confilict (edit github)
   return (
       <div className=''>
         <Body/>
